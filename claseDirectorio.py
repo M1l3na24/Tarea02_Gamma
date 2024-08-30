f985d5141e461169429861d48f75ef6ff86e92c1
Milena <milena_r1vera24@ciencias.unam.mx>
2024-08-30 10:36:21 -0600

    Agregue Readme

---
+++ claseDirectorio.py
@@ -0,0 +1,12 @@
+# Programa: claseDirectorio.py
+# Objetivo: Programa que define la clase Directorio
+# Autores: Milena Rivera, Carlos Barrera, Isaac Garrido, Mayela Rosas
+# Version: 30-08-2024
+
+import numpy as np
+
+class Directorio:
+    ''''
+    clase directorio
+    '''
+    pass
