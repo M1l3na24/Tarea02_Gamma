50cba3380daad37991b5589812f19c6086715fed
Milena <milena_r1vera24@ciencias.unam.mx>
2024-08-30 10:55:18 -0600

    inicie parte 1: constructor directorio

--- claseDirectorio.py
+++ claseDirectorio.py
@@ -6,7 +6,22 @@
 import numpy as np
 
 class Directorio:
-    ''''
-    clase directorio
-    '''
-    pass
+    def __init__(self, almacenamiento):
+        """
+        Constructor que permite crear el objeto Directorio que sera
+        un arreglo que contendra arreglos. Se construye como un directorio
+        nuevo: vacio por default
+        :param almacenamiento: indica el almacenamiento del directorio (0,n)
+        """
+        self.directorio = np.empty(almacenamiento, dtype=object)
+
+    def insertar_nuevo_contacto(self, *params):
+        """
+        Constructor que permite crear diferentes objetos Persona
+        :param params: Lista variable de parámetros (9 o 10)
+        """
+
+        self.alumnos = np.array()
+        self.profesores = np.array()
+        self.coordinadores = np.array()
+
